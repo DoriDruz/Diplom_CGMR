7cea02ae29f1f4130faca514aee182c8ea9ecfe0
Unknown <trek-super007@yandex.ru>
2018-04-30 18:31:55 +0500

    ReWork CGMR with Big Matrix
    In Process (should git before changes - my bad)

---
+++ diplom_1/All_A_in_One.cpp
@@ -0,0 +1,134 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <ctime>
+
+using namespace std;
+
+void main() {
+	string header;
+	string tmp;
+	int size = 134862;
+
+	ifstream A1;
+	ifstream A2;
+	ifstream A3;
+	ifstream A4;
+	ifstream A5;
+	fstream result;
+
+	clock_t begin = clock();
+
+	A1.open("A1.dat");
+	A2.open("A2.dat");
+	A3.open("A3.dat");
+	A4.open("A4.dat");
+	A5.open("A5.dat");
+
+	getline(A1, header);
+	getline(A2, header);
+	getline(A3, header);
+	getline(A4, header);
+	getline(A5, header);
+
+	result.open("result.dat");
+
+	for (int i = 1; i <= size; ++i) {
+		cout << i <<  " / " << size << endl;
+		if (i == 1) {
+			A3 >> tmp;
+			result << tmp << " ";
+			A4 >> tmp;
+			result << tmp << " ";
+			A5 >> tmp;
+			result << tmp << " ";
+			result << "1 0 0 0" << endl; 
+		}
+		else if (i >= 2 && i <= 247) {
+			A2 >> tmp;
+			result << tmp << " ";
+			A3 >> tmp;
+			result << tmp << " ";
+			A4 >> tmp;
+			result << tmp << " ";
+			A5 >> tmp;
+			result << tmp << " ";
+			result << "1 0 0" << endl;
+		}
+		else if (i >= 248 && i <= 494) {
+			A1 >> tmp;
+			result << tmp << " ";
+			A2 >> tmp;
+			result << tmp << " ";
+			A3 >> tmp;
+			result << tmp << " ";
+			A4 >> tmp;
+			result << tmp << " ";
+			A5 >> tmp;
+			result << tmp << " ";
+			result << "1 0" << endl;
+		}
+		else if (i >= 495 && i <= 134368) {
+			result << "1" << " ";
+			A1 >> tmp;
+			result << tmp << " ";
+			A2 >> tmp;
+			result << tmp << " ";
+			A3 >> tmp;
+			result << tmp << " ";
+			A4 >> tmp;
+			result << tmp << " ";
+			A5 >> tmp;
+			result << tmp << " ";
+			result << "1" << endl;
+		}
+		else if (i >= 134369 && i <= 134615) {
+			result << "0 1" << " ";
+			A1 >> tmp;
+			result << tmp << " ";
+			A2 >> tmp;
+			result << tmp << " ";
+			A3 >> tmp;
+			result << tmp << " ";
+			A4 >> tmp;
+			result << tmp << " ";
+			A5 >> tmp;
+			result << tmp << endl;
+		}
+		else if (i >= 134616 && i <= 134861) {
+			result << "0 0 1" << " ";
+			A1 >> tmp;
+			result << tmp << " ";
+			A2 >> tmp;
+			result << tmp << " ";
+			A3 >> tmp;
+			result << tmp << " ";
+			A4 >> tmp;
+			result << tmp << endl;
+		}
+		else if (i == 134862) {
+			result << "0 0 0 1" << " ";
+			A1 >> tmp;
+			result << tmp << " ";
+			A2 >> tmp;
+			result << tmp << " ";
+			A3 >> tmp;
+			result << tmp << endl;
+		}
+	}
+
+	result << "End";
+	result.close();
+
+	A1.close();
+	A2.close();
+	A3.close();
+	A4.close();
+	A5.close();
+
+	clock_t end = clock();
+	double time = double(end - begin) / CLOCKS_PER_SEC;
+	cout << "Time of calc: " << time << endl;
+
+	system("pause");
+}
---
+++ diplom_1/main.cpp
@@ -0,0 +1,342 @@
+// Итерац. МСГ с регуляризацией на процессоре, многоядерном процессоре и видеокарте
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <ctime>
+
+using namespace std;
+
+//global
+const int S = 134862;
+
+void create_matr(ifstream& file, double *matr, double size) {
+	for (int i = 0; i < size; ++i) {
+		file >> matr[i];
+	}
+}
+
+//size = sizeof(matr) ?(!warn about sizeof!)?
+void show_matr(double *matr, double size) {
+	for (int i = 0; i < size; ++i) {
+		cout << matr[i] << "; ";
+	}
+	cout << endl;
+}
+
+//add res_matr?
+void mult_matr_on_num(double *matr, double num, double *res_matr, double size) {
+	for (int i = 0; i < size; ++i) {
+		res_matr[i] = matr[i] * num;
+
+		//debug
+		//cout << "mult matr on num debug: " << res_matr[i] << " = " << matr[i] << " * " << num << endl;
+	}
+}
+
+void mult_A_on_alpha_E(double *matr, double alpha, double *res_matr) {
+	for (int i = 0; i < S; ++i) {
+		if (i == 0) {
+			matr[i] = matr[i] + alpha;
+		}
+		else if (i > 0 && i < 247) {
+			matr[i * 7 + 1] = matr[i] + alpha;
+		}
+		else if (i > 246 && i < 494) {
+			matr[i * 7 + 2] = matr[i] + alpha;
+		}
+		else if (i > 493 && i < 134368) {
+			matr[i * 7 + 3] = matr[i] + alpha;
+		}
+		else if (i > 134367 && i < 134615) {
+			matr[i * 7 + 4] = matr[i] + alpha;
+		}
+		else if (i > 134614 && i < 134861) {
+			matr[i * 7 + 5] = matr[i] + alpha;
+		}
+		else if (i == 134861) { //944034
+			matr[i * 7 + 6] = matr[i] + alpha;
+		}
+	}
+}
+
+void sum_matr(double *matr_one, double *matr_two, double *res_matr, double size) {
+	for (int i = 0; i < size; ++i) {
+		res_matr[i] = matr_one[i] + matr_two[i];
+	}
+}
+
+
+//warn about size! (now for vector's size)
+void dif_matr(double *matr_one, double *matr_two, double *res_matr) {
+	for (int i = 0; i < 3; ++i) {
+		res_matr[i] = matr_one[i] - matr_two[i];
+		
+		//debug
+		//cout << "debug: " << res_matr[i] << " = " << matr_one[i] << " - " << matr_two[i] << endl;
+	}
+}
+
+void matr_on_vec(double *matr, double *vec, double *res_vec) {
+	for (int i = 0; i < 3; ++i) {
+		for (int j = 0; j < 3; ++j) {
+			res_vec[i] += matr[3*i + j] * vec[j];
+
+			//debug
+			//cout << "i: " << i << " j: " << j  << " res_vec: " << res_vec[i] << " matr: " << matr[3 * i + j] << " vec: " << vec[j] <<  endl;
+		}
+	}
+}
+
+double vec_on_vec(double *vec_one, double *vec_two) {
+	double res = 0;
+	for (int i = 0; i < 3; ++i) {
+		res += vec_one[i] * vec_two[i];
+	}
+	//debug
+	//cout << res << endl;
+	//system("pause");
+	
+	return res;
+}
+
+double norm_vec(double *vec) {
+	double res = 0;
+	for (int i = 0; i < 3; ++i) {
+		res += pow(vec[i], 2);
+	}
+	res = sqrt(res);
+	return res;
+}
+
+//size?
+void copy_vec(double *matr_one, double *matr_two) {
+	for (int i = 0; i < 3; ++i) {
+		matr_one[i] = matr_two[i];
+	}
+}
+
+void nullify(double *vec) {
+	for (int i = 0; i < 3; ++i) {
+		vec[i] = 0;
+	}
+}
+//stop deal (rework!)
+double stop_deal(double *rA, double *x_k1, double *F) {
+	double *stop = NULL;
+	double *tmp = NULL;
+	double stop_norm = 0;
+	double stop_norm2 = 0;
+	//rA*x_k
+	matr_on_vec(rA, x_k1, stop);
+	//rA*x_k - b
+	dif_matr(stop, F, tmp);
+	//norm(rA*x_k - b)
+	stop_norm = norm_vec(tmp);
+	//norm(b)
+	stop_norm2 = norm_vec(F);
+	//stop_deal = stop_norm / stop_norm2
+	return (stop_norm / stop_norm2);
+}
+
+void CGMR(double *A, double *F) {
+	const double Eps = 0.1;
+
+	//const double l_size = 9;
+	double *rA = new double[S * 7];
+	double *x = new double[S];
+	double *r = new double[S];
+	double *p = new double[S];
+
+	//coef in cycle
+	double ak = 0;
+	double bk = 0;
+	
+	//const coef a
+	double alpha = 0;
+
+	//tmp
+	double *matr_tmp = new double[S * 7];
+	double *tmp = new double[S];
+	double *r_k1 = new double[S];
+	double *x_k1 = new double[S];
+	double *p_k1 = new double[S];
+	double *stop = new double[S];
+	double stop_norm = 0;
+	double stop_norm2 = 0;
+	double norm = 0;
+	double norm2 = 0;
+	double vov = 0;
+	double stop_eps = 0;
+
+	for (int i = 0; i < S; ++i) {
+		x[i] = r[i] = p[i] = 0;
+		stop[i] = tmp[i] = r_k1[i] = x_k1[i] = p_k1[i] = 0;
+	}
+
+	//making const rA
+	//alpha*E
+	//mult_matr_on_num(E, alpha, matr_tmp, 9);
+	//rA = A + a*E
+	mult_A_on_alpha_E(A, alpha, rA);
+	//sum_matr(A, matr_tmp, rA, 9);
+
+	//rA*x0
+	matr_on_vec(rA, x, tmp);
+	//r = b-rA*x0
+	dif_matr(F, tmp, r);
+	//p = r;
+	copy_vec(p, r);
+
+	//stop deal before cycle
+	//rA*x_k
+	matr_on_vec(rA, x_k1, stop);
+	//rA*x_k - b
+	dif_matr(stop, F, tmp);
+	//norm(rA*x_k - b)
+	stop_norm = norm_vec(tmp);
+	//norm(b)
+	stop_norm2 = norm_vec(F);
+	//stop_deal = stop_norm / stop_norm2
+	stop_eps = (stop_norm / stop_norm2);
+
+
+	//mb norm like scalar not norm (|| ||) itself?
+	while( !(stop_eps < Eps) ) {
+		cout << "Start of iter" << endl;
+		
+		nullify(tmp);
+		//ak = norm(r_k)^2 / (rA*p_k, p_k)
+		//norm(r_k)^2
+			//norm = pow(norm_vec(r), 2);
+		norm = vec_on_vec(r, r);
+		//rA*p_k
+		matr_on_vec(rA, p, tmp);
+		//(rA*p_k, p_k)
+		vov = vec_on_vec(tmp, p);
+		//ak = ...
+		ak = (isnan(norm / vov) ? 0 : (norm / vov));
+		
+		nullify(tmp);
+		//r_k+1 = r_k - ak*rA*p_k
+		//rA*p_k
+		matr_on_vec(rA, p, tmp);
+			//cout << "rA*pk: " << endl;
+			//show_matr(tmp, 3);
+		//ak*(rA*p_k)
+		mult_matr_on_num(tmp, ak, tmp, 3);
+			//cout << "ak*rA*pk: " << endl;
+			//show_matr(tmp, 3);
+		//r_k+1 = r_k - ...
+		dif_matr(r, tmp, r_k1);
+
+		//cout << "r_k: " << endl;
+		//show_matr(r, 3);
+		//cout << "r_k+1:" << endl;
+		//show_matr(r_k1, 3);
+
+		nullify(tmp);
+		//x_k+1 = x_k + ak*p_k
+			//tmp = p;
+			//copy_vec(tmp, p);
+		//ak*p_k
+		mult_matr_on_num(p, ak, tmp, 3);
+		//x_k+1 = x_k + ...
+		sum_matr(x, tmp, x_k1, 3);
+
+		//bk = norm(r_k+1)^2 / norm(r_k)^2
+		//norm(r_k+1)^2
+			//norm = pow(norm_vec(r_k1), 2);
+		norm = vec_on_vec(r_k1, r_k1);
+		//norm(r_k)^2
+			//norm2 = pow(norm_vec(r), 2);
+		norm2 = vec_on_vec(r, r);
+		//bk = ...
+		bk = (isnan(norm / norm2) ? 0 : norm / norm2);
+
+		nullify(tmp);
+		//p_k+1 = r_k+1 + bk*p_k
+		//bk*p_k
+			//tmp = p;
+			//copy_vec(tmp, p);
+		mult_matr_on_num(p, bk, tmp, 3);
+		//p_k+1 = r_k+1 + ...
+		sum_matr(r_k1, tmp, p_k1, 3);
+
+		//cout << "p_k: " << endl;
+		//show_matr(p, 3);
+		//cout << "p_k+1: " << endl;
+		//show_matr(p_k1, 3);
+		
+		//show results
+		cout << "coefs:" << endl << "alpha: " << alpha << "; ak: " << ak << "; bk: " << bk << endl;
+
+		cout << "x_k1: " << endl;
+		show_matr(x_k1, 3);
+
+		nullify(tmp);
+		//stop deal
+		//rA*x_k
+		matr_on_vec(rA, x_k1, stop);
+		//rA*x_k - b
+		dif_matr(stop, F, tmp);
+		//norm(rA*x_k - b)
+		stop_norm = norm_vec(tmp);
+		//norm(b)
+		stop_norm2 = norm_vec(F);
+		//stop_deal = stop_norm / stop_norm2
+		stop_eps = (stop_norm / stop_norm2);
+
+		cout << "Stop deal: " << stop_eps << " " << ((stop_eps < Eps) ? "<" : ">") << " " << Eps << endl;
+		//boolalpha << 
+
+		//copy for next iter
+		//p = p_k1;
+		copy_vec(p, p_k1);
+		//x = x_k1;
+		copy_vec(x, x_k1);
+		//r = r_k1;
+		copy_vec(r, r_k1);
+		//clear stop
+		nullify(stop);
+		nullify(p_k1);
+		nullify(x_k1);
+		nullify(r_k1);
+		cout << "End of iter" << endl << endl;
+	}
+
+}
+
+void create_big_matrix() {
+	
+}
+
+void main() {
+
+	//const double matr_size = 9;
+	//double *matr_A = new double[matr_size];
+	//double *matr_E = new double[matr_size];
+	//double *matr_F = new double[matr_size / 3];
+
+	double *matr_A = new double[S * 7];
+	double *matr_F = new double[S];
+
+	ifstream A;
+	ifstream F;
+	ifstream E;
+
+	clock_t begin = clock();
+
+	A.open("result.dat");	// 134862 * 3
+	F.open("F.dat");		// 134862
+
+	create_matr(A, matr_A, S * 7);
+	create_matr(F, matr_F, S);
+
+	CGMR(matr_A, matr_F);
+
+	clock_t end = clock();
+	double time = double(end - begin) / CLOCKS_PER_SEC;
+	cout << "Time of calc: " << time << endl;
+
+	system("pause");
+}
